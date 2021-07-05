b4b29e6c17f37ac780060ed8fd9926056dc7893e
dobri <dobri@DESKTOP-V3T1G40>
2021-07-05 15:33:32 +0300

    Сделай форму

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace VK
 {
@@ -15,6 +16,18 @@ namespace VK
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\dobri\OneDrive\Рабочий стол\Новая папка\Nado.txt");
+            textBox1.Text = sr.ReadToEnd();
+            sr.Close();
         }
     }
 }
